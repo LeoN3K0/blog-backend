56d201a1b69366e2c825a330b8952324f4902d17
LeoN3K0 <emalberg@live.com>
2023-06-30 07:45:09 -0600

    removed annotations that were not needed

--- src/main/java/com/erichmalberg/blogbackend/repository/RoleRepository.java
+++ src/main/java/com/erichmalberg/blogbackend/repository/RoleRepository.java
@@ -3,13 +3,11 @@ package com.erichmalberg.blogbackend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.erichmalberg.blogbackend.models.ERole;
 import com.erichmalberg.blogbackend.models.Role;
 
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
   Optional<Role> findByName(ERole name);
 }
--- src/main/java/com/erichmalberg/blogbackend/repository/UserRepository.java
+++ src/main/java/com/erichmalberg/blogbackend/repository/UserRepository.java
@@ -3,11 +3,10 @@ package com.erichmalberg.blogbackend.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.erichmalberg.blogbackend.models.User;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByUsername(String username);
 
